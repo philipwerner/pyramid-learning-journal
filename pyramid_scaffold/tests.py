14ce4a19624d015a15be8f62d0c5ace1f9830378
Philip Werner <philip.r.werner@gmail.com>
2017-11-05 11:00:32 -0800

    all done

--- pyramid_scaffold/tests.py
+++ pyramid_scaffold/tests.py
@@ -3,7 +3,7 @@ from pyramid_scaffold.models import Entry, get_tm_session
 from pyramid_scaffold.models.meta import Base
 from pyramid.testing import DummyRequest
 from datetime import datetime
-from pyramid.httpexceptions import HTTPNotFound, HTTPFound
+from pyramid.httpexceptions import HTTPNotFound, HTTPFound, HTTPBadRequest
 from pyramid import testing
 import pytest
 import transaction
@@ -96,3 +96,106 @@ def test_detail_view_non_existent_entry(dummy_request):
     dummy_request.matchdict['id'] = 2
     with pytest.raises(HTTPNotFound):
         detail_view(dummy_request)
+
+
+def test_create_view_makes_new_entry(dummy_request):
+    """Test new create view makes new entry."""
+    from pyramid_scaffold.views.default import create_view
+    entry_info = {
+        "title": "New entry",
+        "body": "This is the body"
+    }
+    dummy_request.method = "POST"
+    dummy_request.POST = entry_info
+    create_view(dummy_request)
+    entry = dummy_request.dbsession.query(Entry).first()
+    assert entry.title == "New entry"
+
+
+def test_create_view_on_post_redirects_to_home_page(dummy_request):
+    """Test new entry will redirect to the home page."""
+    from pyramid_scaffold.views.default import create_view
+    entry_info = {
+        "title": "New entry",
+        "body": "This is the body"
+    }
+    dummy_request.method = "POST"
+    dummy_request.POST = entry_info
+    response = create_view(dummy_request)
+    assert isinstance(response, HTTPFound)
+
+
+def test_create_view_incomplete_data_placeholder_text(dummy_request):
+    """Test an incomplete POST will return a HTTPBadRequest."""
+    from pyramid_scaffold.views.default import create_view
+    entry_info = {
+        "title": 'Test title',
+    }
+    dummy_request.method = "POST"
+    dummy_request.POST = entry_info
+    with pytest.raises(HTTPBadRequest):
+        create_view(dummy_request)
+
+
+@pytest.fixture(scope="session")
+def testapp(request):
+    """Test app for learning journal tests."""
+    from webtest import TestApp
+    from pyramid.config import Configurator
+
+    def main():
+        settings = {
+            'sqlalchemy.url': 'postgres://localhost:5432/pyramid_scaffold'
+        }
+        config = Configurator(settings=settings)
+        config.include('pyramid_jinja2')
+        config.include('pyramid_scaffold.routes')
+        config.include('pyramid_scaffold.models')
+        config.scan()
+        return config.make_wsgi_app()
+
+    app = main()
+
+    session_factory = app.registry["dbsession_factory"]
+    engine = session_factory().bind
+    Base.metadata.create_all(bind=engine)
+
+    def tearDown():
+        Base.metadata.drop_all(bind=engine)
+
+    request.addfinalizer(tearDown)
+
+    return TestApp(app)
+
+
+@pytest.fixture(scope="session")
+def fill_the_db(testapp):
+    """Fill the db for the testapp."""
+    session_factory = testapp.app.registry["dbsession_factory"]
+    with transaction.manager:
+        dbsession = get_tm_session(session_factory, transaction.manager)
+        dbsession.add_all(ENTRIES)
+
+ENTRIES = []
+for i in range(1, 20):
+    new_entry = Entry(
+        title='Test Journal {}'.format(i),
+        body='Test body'
+    )
+    ENTRIES.append(new_entry)
+
+
+def test_delete_has_deleted_data(testapp, fill_the_db):
+    """Test that delete will delete a post."""
+    response = testapp.get("/journal/3/delete")
+    assert 'Test Journal 3' not in response
+
+
+def test_create_view_successful_post_redirects_home(testapp):
+    """Test that a new post will redirect to the list view (home page)."""
+    entry_info = {
+        "title": "Test title",
+        "body": "Test body"
+    }
+    response = testapp.post("/create", entry_info)
+    assert response.location == 'http://localhost/'
