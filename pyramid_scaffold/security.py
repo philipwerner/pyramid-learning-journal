79c29f1a07faf8424dd7170d9f0f171d5d1a87aa
Philip Werner <philip.r.werner@gmail.com>
2017-11-19 14:09:51 -0800

    working on cookies and eating real cookies

--- pyramid_scaffold/security.py
+++ pyramid_scaffold/security.py
@@ -4,6 +4,7 @@ from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.security import Authenticated
 from pyramid.security import Allow
+from pyramid.session import SignedCookieSessionFactory
 from passlib.apps import custom_app_context as pwd_context
 
 
@@ -34,7 +35,10 @@ def includeme(config):
         secret=auth_secret,
         hashalg='sha512'
     )
-    config.set_authentication_policy(authn_policy)
+    session_secret = os.environ.get('SESSION_SECRET', 'itsaseekrit')
+    session_factory = SignedCookieSessionFactory(session_secret)
+    config.set_session_factory(session_factory)
+    config.set_default_csrf_options(require_csrf=True)
     authz_policy = ACLAuthorizationPolicy()
     config.set_authorization_policy(authz_policy)
     config.set_root_factory(MyRoot)
